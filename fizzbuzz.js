4d36a04e6e03241986a838022e784c5ab5779778
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-06-13 21:16:37 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,12 @@
+/*
+Function doesn't need any parameters.
+The range is established in the for loop starting with index (i) as 1 and going to less than or equal to 100. Then i++ counts through it by 1 number.
+In the if statement, you need to start with the outlier, fizzbuzz, before moving onto fizz and buzz.
+Else just prints out the count.
+No return needed because it's console.log'd at every level
+*/
+
+
 function fb(){
     for (let i = 1; i <= 100; i++){
         if (i % 3 === 0 && i % 5 === 0){
